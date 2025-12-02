377f9a255fa57d1936fea33c10059bee558c94fc
Nader Elsayed <79558954+Nadoura-boop@users.noreply.github.com>
2025-12-03 00:36:49 +0200

    last

--- main.js
+++ main.js
@@ -1,44 +1,56 @@
-
 const time = document.getElementById("time");
 const start = document.getElementById("start");
 const stopBtn = document.getElementById("stop");
 const reset = document.getElementById("reset");
+const lab = document.getElementById("lab");
+const labs = document.getElementById("labs");
 
 let seconds = 0;
 let timer = null;
-function formatTime(sec){
+
+function formatTime(sec) {
     let hrs = Math.floor(sec / 3600);
     let mins = Math.floor((sec % 3600) / 60);
     let secs = sec % 60;
     return `${hrs.toString().padStart(2,"0")}:${mins.toString().padStart(2,"0")}:${secs.toString().padStart(2,"0")}`;
 }
 
-start.onclick = () => {
-    if(timer !== null) return; 
+
+start.addEventListener("click", () => {
+    if (timer !== null) return;
 
     timer = setInterval(() => {
         seconds++;
-        time.innerHTML = formatTime(seconds);
+        time.textContent = formatTime(seconds);
     }, 1000);
-};
+});
 
-stopBtn.onclick = () => {
-    if(timer !== null){ 
+stopBtn.addEventListener("click", () => {
+    if (timer !== null) {
         clearInterval(timer);
         timer = null;
         stopBtn.textContent = "Resume";
     } else {
-        timer = setInterval(()=>{
+        timer = setInterval(() => {
             seconds++;
-            time.innerHTML = formatTime(seconds);
-        },1000);
+            time.textContent = formatTime(seconds);
+        }, 1000);
         stopBtn.textContent = "Stop";
     }
-};
+});
 
-reset.onclick = () => {
+reset.addEventListener("click", () => {
     clearInterval(timer);
     timer = null;
     seconds = 0;
-    time.innerHTML = formatTime(seconds);
-};
+    labs.innerHTML='';
+    time.textContent = formatTime(seconds);
+});
+
+lab.addEventListener("click",()=>{
+    if (timer !== null) {
+        const li=document.createElement('li')
+        li.textContent=time.textContent;
+        labs.appendChild(li)
+    }
+})
